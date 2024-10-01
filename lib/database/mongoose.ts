5e5d00adfd9d452d23c893216af5069ea5fd868e
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-10-02 03:36:39 +0600

    es lint error solved

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,3 +1,39 @@
+// import mongoose, { Mongoose } from "mongoose";
+
+// const MONGODB_URL = process.env.MONGODB_URL;
+
+// interface MongooseConnection {
+//   conn: Mongoose | null;
+//   promise: Promise<Mongoose> | null;
+// }
+
+// let cached: MongooseConnection = (global as any).mongoose;
+
+// if (!cached) {
+//   cached = (global as any).mongoose = {
+//     conn: null,
+//     promise: null,
+//   };
+// }
+
+// export const connectToDatabase = async () => {
+//   if (cached.conn) return cached.conn;
+
+//   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+//   cached.promise =
+//     cached.promise ||
+//     mongoose.connect(MONGODB_URL, {
+//       dbName: "imaginify",
+//       bufferCommands: false,
+//     });
+
+//   cached.conn = await cached.promise;
+
+//   return cached.conn;
+// };
+
+/* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose, { Mongoose } from "mongoose";
 
@@ -8,17 +44,19 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+// Extend the global object to add mongoose connection caching
+declare global {
+  var mongoose: MongooseConnection | undefined;
+}
+
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  if (cached?.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
